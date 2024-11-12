b64776863eb4853de0f50e2f41b693f1d632c1d0
Sergey Zhdanov <serguchozhdanov@yandex.ru>
2024-11-12 12:25:20 +0700

    last try(second)

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -10,7 +10,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class Mesto1Test {
 
-    String bearerToken = "сюда_впиши_свой_токен";
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzMyZTY0ZjNhMDdkZjAwM2QxMjJiMjAiLCJpYXQiOjE3MzEzODkwMDcsImV4cCI6MTczMTk5MzgwN30.RO0ek8UEBiwbfdkPHq7194hrlQmWtL_WNfdpCRxsKuc";
 
     @Before
     public void setUp() {
